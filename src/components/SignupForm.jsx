54ee52d2e350ba75874da190a1ebf75ee59587ed
vikesh jha <vikeshjha39@gmail.com>
2025-07-23 15:40:39 +0530

    first commit

---
+++ src/components/SignupForm.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+
+const SignupForm = () => {
+
+    const[form, setForm] = useState({ name: '', password: ''})
+    const[msg, setMsg] = useState('');
+
+    const handleChange = (e) =>{
+        setForm({...form , [e.target.name]: e.target.value});
+    };
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+        try{
+            const res = await axios.post('http://localhost:5000/signup', form);
+            setMsg(res.data.message);
+        }catch (err) {
+            setMsg("Error creating user");
+        }
+    };
+  return (
+    <div>
+        <h2>Sign Up</h2>
+        <form onSubmit={handleSubmit}>
+            <input
+            name='name'
+            value={form.name}
+            onChange={handleChange}
+            placeholder='Name'
+            required
+            />
+            <input
+            name='password'
+            type='password'
+            value={form.password}
+            onChange={handleChange}
+            placeholder='Password'
+            required
+            />
+            <button type='submit'>Submit</button>
+        </form>
+        {msg && <p>{msg}</p>}
+    </div>
+  )
+}
+
+export default SignupForm
